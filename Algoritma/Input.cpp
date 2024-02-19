8423aec04938376037b1c279c0ca69109f4f92a9
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2024-02-19 14:10:55 +0700

    menambahkan catatan algoritma dasar c #1

---
+++ Algoritma/Input.cpp
@@ -0,0 +1,20 @@
+// #include<stdio.h>
+
+// int main(){
+// 	// Input -> scanf()
+// 	// angka , string
+// 	// contohnya mau input nama dan umur
+// 	char nama[50];
+// 	int umur;
+
+// 	printf("Masukkan nama kamu : ");
+// 	scanf("%[^\n]" , nama);fflush(stdin); // menghapus buffer yang ada di scan f string nama
+
+// 	printf("Masukkan umur kamu : ");
+// 	scanf("%d" , &umur);fflush(stdin);
+
+// 	printf("\n\nNama kamu adalah %s\n" , nama);
+// 	printf("Umur kamu adalah %d" , umur);
+
+// 	return 0;
+// }
---
+++ Algoritma/variabelOutput.cpp
@@ -0,0 +1,30 @@
+// #include<stdio.h> //  dev c memili banyak library, library itu sekumpulan komen komen yang bisa kita gunakan
+// // contohnya stdio.h singkatan dari standard input dan output. header
+
+// int main(){
+// 	// variabel
+// 	// angka , kata, koma
+// 	// int = integer
+// 	int angka;
+// 	angka = 10;
+
+// 	// karakter
+// 	char karakter;
+// 	karakter = '!';
+
+// 	// koma
+// 	float bilangan;
+// 	bilangan = 3.5;
+
+// 	// printf() -> cara enter \n
+// 	// cara komentar : di block dulu , lalu ctrl + /
+// 	// printf("angka berisi %d\n" , angka);
+// 	// printf("karakter berisi %c\n" , karakter);
+// 	// printf("bilangan berisi %f" , bilangan);
+
+// 	char nama[50]= "andi wandi";
+
+// 	printf("nama saya adalah %s", nama); //string copy
+
+// 	return 0; // berfungsi untuk jalan kan kodingan agar tidak error
+// }
