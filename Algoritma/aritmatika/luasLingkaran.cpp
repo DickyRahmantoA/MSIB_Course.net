e0bda3cf8353a83308b0fbfb0279593cd23ddd57
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2024-02-19 17:58:52 +0700

    menambahkan catatan algoritma dasar c #2

---
+++ Algoritma/aritmatika/luasLingkaran.cpp
@@ -0,0 +1,17 @@
+// #include<stdio.h>
+//
+//int main(){
+//	// phi 3.14
+//	const float phi = 3.14; // tipe data disebelah kanan const sudah tidak akan bisa diubah alias permanen
+//	float jariJari;
+//	float luas;
+//	
+//	
+//	printf("masukkan jari jari lingkaran : ");
+//	scanf("%f", &jariJari);
+//	
+//	luas = phi * jariJari * jariJari;
+//	printf("Luas lingkaran ialah %.2f\n" , luas);
+//	
+//	return 0;
+//}
---
+++ Algoritma/aritmatika/modulusIncrement.cpp
@@ -0,0 +1,47 @@
+//#include<stdio.h>
+//
+//int main(){
+	// Operasi Aritmatika
+	// Modulus % 
+//	int permen = 18;
+//	int sisa;
+//	sisa = permen % 5;
+//	
+//	printf("sisa permen adalah %d", sisa);
+	
+	// Increment ++ dan Decrement -- -> membantu menambahkan dan mengurangi 1
+	// PreIncrement ++angka , PostIncrement angka++
+//	int angka = 10;
+//	printf("angka sebelum ++ %d\n" , angka++); // 10
+//	printf("angka setelah ++ %d\n" , angka); // 11
+	
+	// bahasa c itu sekuensial pembacaan kodenya dari atas kebawah
+	// operasi aritmatika
+//	int angka1 = 10;
+//	int angka2 = 20;
+//	
+//	int hasil = angka1+angka2; // 30
+//	
+//	printf("Angka1+angka2 = %d" , angka1+angka2); // cara 1
+//	printf("Angka1+angka2 = %d\n\n" , hasil); // cara 2
+//	
+//	hasil = angka2-angka1;
+//	
+//	printf("Hasil Sekarang adalah = %d\n\n" , hasil);
+	
+//	float alas;
+//	float tinggi;
+//	float hasil;
+//	
+//	printf("Masukkan alas: ");      // input
+//	scanf("%f" , &alas);
+//	
+//	printf("Masukkan tinggi: "); 
+//	scanf("%f" , &tinggi);
+//	
+//	hasil = (alas*tinggi) / 2;
+//	
+//	printf("Luas Segitiga ialah %.2f" , hasil);
+//	
+//	return 0;
+//}
