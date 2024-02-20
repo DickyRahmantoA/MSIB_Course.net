3fef8b9125b9d76e397cc60b5bc67a7704edd37d
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2024-02-20 16:07:27 +0700

    menambahkan catatan algoritma dasar c #4

---
+++ Algoritma/Repetition/looping.cpp
@@ -0,0 +1,49 @@
+// #include <stdio.h>
+
+// int main()
+// {
+// Looping (Perulangan Repetisi)
+// do while , while , for
+// do while, dipakai ketika kita pasti melakukan statement/code minimal 1x.
+// tidak melihat kondisi terlebih dahulu jadi jalanin kodingan baru melihat kondisi
+
+// while, dipakai ketika kita mau melakukan repetisi tapi dengan kondisi terlebih dahulu
+// melihat kondisi terlebih dahulu
+
+// for, dipakai ketika kita mengetahui betul kapan kita mau looping dan kapan kita mau selesai looping
+
+// int angka = 10;
+
+// for(int i=0 ; i < 10 ; i++ ){  // inisialisasi angka, kondisi, increment/decrement
+// 	printf("angka : %d\n" , angka);
+// }
+
+//	printf("Masukkan angka : ");
+//	scanf("%d" , &angka); fflush(stdin);
+//
+//	while(angka < 10){
+//		printf("%d\n"), angka;
+//		angka++;
+//	}
+//
+//	printf("program selesai");
+
+//	do{
+//		// statement
+//		printf("1.Menu");
+//		printf("2.Exit");
+//		printf("Pilih Menu : ");
+//		scanf("%d", &input); ffflush(stdin);
+//
+//		switch(input){
+//			case 1:
+//				printf("Anda memasuki menu1\n");
+//				break;
+//		}
+//
+//	}while(input != 2); // condition jika kondisi benar maka dia akan mengulang
+//
+//	printf("Program selesai\n");
+
+//     return 0;
+// }
