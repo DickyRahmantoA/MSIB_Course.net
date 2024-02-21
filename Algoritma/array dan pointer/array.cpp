d8cf7c37e0e0c3fb24a559aba04f3f5a924ea84b
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2024-02-21 11:10:22 +0700

    menambahkan catatan algoritma dasar c #5

---
+++ Algoritma/array dan pointer/array.cpp
@@ -0,0 +1,25 @@
+//#include <stdio.h>
+	// pointer, array, struct
+//int main()
+//{
+	// array 1 dimensi
+//	int angka[4] = {5,6,7,8};
+//	printf("isi dari array angka : ");
+//	for(int i = 0; i < 4; i++){
+//		printf("%d", angka[i]);
+//	}
+	
+	// array 2 dimensi
+//	int angka[2][4] = { {1,2,3,4},
+//						{5,6,7,8} };
+//	for(int j = 0; j < 2; j++){
+//		printf("isi dari baris ke - %d : ", j);
+//		for(int i = 0; i < 4; i++){
+//			printf("%d", angka[j][i]);
+//		}
+//		printf("\n")	;
+//	}
+	
+	
+//    return 0;
+//}
---
+++ Algoritma/array dan pointer/pointer.cpp
@@ -0,0 +1,24 @@
+//#include <stdio.h>
+
+//int main()
+//{
+	
+	// pointer
+	// * menunjukkan isi dari
+	// & menunjukkan isi alamat
+	
+//	int angka = 100;
+//	int *ptr; // penunjuk dan spesial nya bisa bergabung dengan angka
+	
+//	ptr = &angka;
+//	printf("isi dari angka : %d\n" , angka);
+//	printf("isi dari angka : %d\n", &angka);
+//	printf("isi dari angka : %d\n" , &ptr);
+//	printf("isi dari *ptr : %d\n" , *ptr);
+//	printf("isi dari ptr : %d\n" , ptr);
+	
+//	*ptr = 30;
+//	printf("isi dari angka : %d\n" , angka);
+
+//    return 0;
+//}
