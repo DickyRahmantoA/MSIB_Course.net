7048f654decd479c79cdbb1a7135a8d90f2cbd61
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2024-02-22 21:57:27 +0700

    menambahkan catatan algoritma dasar c #7

---
+++ Algoritma/recursive and struct/recursive.cpp
@@ -0,0 +1,21 @@
+//#include <stdio.h>
+//
+//int factorial(int n) {
+//	if(n == 0 || n == 1){
+//		return 1;
+//	} else {
+//		return n * factorial( n - 1 );
+//	}
+//}
+//
+//int main()
+//{
+//	// recursive
+//	// adalah function yang memanggil dirinya sendiri
+//	
+//	int hasil = factorial(3);
+//	
+//	printf("Hasil factorial = %d" , hasil);
+//
+//    return 0;
+//}
