f889bdaef5450201aec417128143bece80892416
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2024-02-23 10:57:28 +0700

    menambahkan catatan algoritma dasar c #8

---
+++ Algoritma/sorting dan searching/searching.cpp
@@ -0,0 +1,33 @@
+//#include <stdio.h>
+
+//int data[6] = {5, 1, 4, 3, 2};
+//int n = 5;
+
+//int searchAngka(int input){
+//	  for (int i = 0; i < 5; i++)
+//    {
+//        if (input == data[i])
+//        {
+//            return i;
+//        }
+//    }
+//    return -1;
+//}
+
+//int main()
+//{
+    // search
+    // pencarian data dalam sebuah kumpulan
+
+//    int input = 10;
+//    int index = searchAngka(input);
+    
+//    if(index < 0){
+//    	printf("tidak ketemu");
+//	} else {
+//		printf("Ketemu di index-%d", index);
+//	}
+  
+
+//    return 0;
+//}
---
+++ Algoritma/sorting dan searching/sorting.cpp
@@ -0,0 +1,50 @@
+// #include <stdio.h>
+
+//void swap(int &x, int &y) {
+//	int temp;
+//	
+//	temp = x;
+//	x = y;
+//	y = temp;
+//}
+
+//void bubbleSort(int data[], int n){
+//	for(int i = 0; i < n - 1; i++){
+//		
+//		// elemen i terakhir yang sudah ada
+//		for(int j = 0; j < n - i - 1; j++){
+//			
+//			if(data[j] > data [j+1]){
+//				swap(data[j], data[j+1]);
+//			}
+//		}
+//	}
+//}
+
+//void showSortedData(int data[], int n){
+//	for(int i = 0; i < n; i++){
+//		printf("%d " , data[i]);
+//	}
+//	printf("\n");
+//}
+
+//int main()
+//{
+	
+	// sorting - bubble sort
+	// algoritma sorting paling simple dimana sorting ini bekerja
+	// dengan cara men - swap element yang bersebelahan
+	// jika pada urutan yang salah
+	
+//	int data[] = {5, 1, 4, 3, 2};
+//	int n = 5;
+	
+//	bubbleSort(data, n);
+	
+	
+//	showSortedData(data, n);
+	
+	
+	
+//    return 0;
+//}
